db812309f3cccce672247bc26e1175e3540150fc
SimJJ96 <simjingjie13@gmail.com>
2022-09-04 22:17:45 +0800

    Exercises 1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 const Header = (header) => {
   return (
     <h1>
-      {header.course}
+      {header.course.name}
     </h1>
   )
 }
@@ -17,40 +17,46 @@ const Part = (content) => {
 const Content = (content) => {
   return (
     <div>
-      <Part part={content.parts[0]} exercise={content.exercises[0]} />
-      <Part part={content.parts[1]} exercise={content.exercises[1]} />
-      <Part part={content.parts[2]} exercise={content.exercises[2]} />
+      <Part part={content.parts.parts[0].name} exercise={content.parts.parts[0].exercises} />
+      <Part part={content.parts.parts[1].name} exercise={content.parts.parts[1].exercises} />
+      <Part part={content.parts.parts[2].name} exercise={content.parts.parts[2].exercises} />
     </div>
   )
 }
 
 const Total = (content) => {
   return (
+
     <p>
-      Number of exercises {content.exercises[0] + content.exercises[1] + content.exercises[2]}
+      Number of exercises {content.parts.parts[0].exercises + content.parts.parts[1].exercises + content.parts.parts[2].exercises}
     </p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    'Fundamentals of React',
-    'Using props to pass data',
-    'State of a component'
-  ]
-
-  const exercises = [
-      10,
-      7,
-      14
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} exercises={exercises} />
-      <Total exercises={exercises} />
+      <Content parts={course} />
+      <Total parts={course} />
     </div>
   )
 }
